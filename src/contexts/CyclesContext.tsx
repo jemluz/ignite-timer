b8057d09616259f146dcd15e478b6fd46f11a42b
Jemima <contatojemima@gmail.com>
2023-01-31 20:33:16 -0300

    Reducers - Salvando estado no storage

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,10 +1,10 @@
-import { createContext, useReducer, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
+import { createContext, useEffect, useReducer, useState } from 'react'
 import {
   CreateCycleData,
   Cycle,
   CyclesContextData,
   CyclesContextProviderProps,
-  CyclesState,
 } from '../models/Cycles.interfaces'
 import {
   addNewCycleAction,
@@ -15,18 +15,49 @@ import { cyclesReducer } from '../reducers/cycles/reducer'
 
 export const CyclesContext = createContext({} as CyclesContextData)
 
+const localStorageName = '@ignite-timer:cycles-state-1.0.0'
+
+function getStateFromLocalStorage() {
+  const storedStateAsJSON = localStorage.getItem(localStorageName)
+
+  if (storedStateAsJSON) {
+    return JSON.parse(storedStateAsJSON)
+  }
+}
+
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storedStateAsJSON = localStorage.getItem(localStorageName)
+
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+    },
+  )
+
+  const { cycles, activeCycleId } = cyclesState
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+    }
+
+    return 0
   })
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
-  const { cycles, activeCycleId } = cyclesState
-
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+    localStorage.setItem(localStorageName, stateJSON)
+  }, [cyclesState])
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds)
